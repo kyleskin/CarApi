859f4679bd819e26e2f211f5e38b78470c45644c
Kyle Skinner <kyleskin@gmail.com>
2022-08-24 10:39:52 -0600

    add default values to Car model to get rid of nullable warnings

--- Car.cs
+++ Car.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace CarApi;
 public class Car
 {
     public Guid Id { get; set; }
-    public string Make { get; set; }
-    public string Model { get; set; }
+    [Required]
+    public string Make { get; set; } = string.Empty;
+    [Required]
+    public string Model { get; set; } = string.Empty;
+    [Required]
     public int Year { get; set; }
-    public string Vin { get; set; }
-    public string Color { get; set; }
+    [Required]
+    public string Vin { get; set; } = string.Empty;
+    [Required]
+    public string Color { get; set; } = string.Empty;
 }
